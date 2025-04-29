0f1d386b3c4c5f41bbfda7d897ce1de16ff83a47
Bashi Imalsha <it22296764@my.sliit.lk>
2025-04-29 16:03:16 +0530

    update

--- client/pages/AddBudgetScreen.js
+++ client/pages/AddBudgetScreen.js
@@ -180,7 +180,7 @@ const styles = StyleSheet.create({
   pickerContainer: { borderWidth: 1,borderColor: '#ddd',borderRadius: 10,marginBottom: 5,overflow: 'hidden', backgroundColor: '#fff',marginTop:5},
   picker: { height: 54, width: "100%" },
   dateButton: { backgroundColor: "#ddd", padding: 12, borderRadius: 5, alignItems: "center", marginVertical: 5 },
-  dateButtonText: { fontSize: 16 },
+  dateButtonText: { fontSize: 12 },
   saveButton: { backgroundColor: "#2e7d32", paddingVertical: 12, borderRadius: 5, alignItems: "center",marginTop: 10 ,marginBottom:10 },
   saveButtonText: { color: "#fff", fontSize: 14, fontWeight: "bold" },
   budgetItem: { backgroundColor: "#fff", padding: 10, borderRadius: 5, marginVertical: 5, elevation: 3 },
